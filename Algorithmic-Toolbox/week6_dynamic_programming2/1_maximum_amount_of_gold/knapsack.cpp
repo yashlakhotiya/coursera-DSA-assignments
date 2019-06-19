bcd213f59fb31e44a713ff80754792c6e869128c
Prakirti Lakhotiya <lakhotiyayash@gmail.com>
2019-06-19 16:32:35 +0530

    added week 6

--- Algorithmic-Toolbox/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.cpp
+++ Algorithmic-Toolbox/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.cpp
@@ -3,22 +3,40 @@
 
 using std::vector;
 
-int optimal_weight(int W, const vector<int> &w) {
+long long optimal_weight(long long W, const vector<long long> &w) {
   //write your code here
-  int current_weight = 0;
-  for (size_t i = 0; i < w.size(); ++i) {
-    if (current_weight + w[i] <= W) {
-      current_weight += w[i];
-    }
+  //long long current_weight = 0;
+  long long n = W+1;
+  long long m = w.size()+1;
+  vector<long long> arr(n * m);
+  for(long long i = 0; i < m; i++){
+  	for(long long j = 0;j < n; j++){
+  		if(i == 0 || j == 0){
+  			arr[i * n + j] = 0;
+  		}
+  		else if(w[i-1] > j){
+  			arr[i * n + j] = arr[(i-1) * n + j];
+  		}
+  		else{
+  			arr[i * n + j] = std::max(arr[(i-1)*n+j-w[i-1]]+w[i-1],arr[(i-1) * n + j]);
+  		}
+  	}
   }
-  return current_weight;
+ /*for(size_t i = 0; i < m; i++){
+  	for(size_t j = 0;j < n; j++){
+  		std::cout<<arr[i][j]<<"\t";
+  	}
+  	std::cout<<std::endl;
+  }*/
+  
+  return arr[m*n-1];
 }
 
 int main() {
-  int n, W;
+  long long n, W;
   std::cin >> W >> n;
-  vector<int> w(n);
-  for (int i = 0; i < n; i++) {
+  vector<long long> w(n);
+  for (long long i = 0; i < n; i++) {
     std::cin >> w[i];
   }
   std::cout << optimal_weight(W, w) << '\n';
--- Algorithmic-Toolbox/week6_dynamic_programming2/3_maximum_value_of_an_arithmetic_expression/placing_parentheses.cpp
+++ Algorithmic-Toolbox/week6_dynamic_programming2/3_maximum_value_of_an_arithmetic_expression/placing_parentheses.cpp
@@ -2,6 +2,9 @@
 #include <cassert>
 #include <string>
 #include <vector>
+#include <cstring>
+#include <climits>
+#include <cmath>
 
 using std::vector;
 using std::string;
@@ -22,7 +25,33 @@ long long eval(long long a, long long b, char op) {
 
 long long get_maximum_value(const string &exp) {
   //write your code here
-  return 0;
+	int n = exp.size();
+	size_t no = (n+1)/2;
+	long long min_arr[n][n];
+	long long max_arr[n][n];
+
+	for(size_t i = 0;i < no;i++){
+		min_arr[i][i] = std::stoll(exp.substr(2*i,1));
+		max_arr[i][i] = std::stoll(exp.substr(2*i,1));
+	}
+	for(size_t s = 0; s < no-1; s++){
+		for(size_t i = 0; i < no - s - 1; i++){
+			long long minVal = LLONG_MAX;
+			long long maxVal = LLONG_MIN;
+			size_t j = i+s+1;
+			for(size_t k = i;k < j;k++){
+				long long a = eval(min_arr[i][k],min_arr[k+1][j],exp[2*k+1]);
+				long long b = eval(min_arr[i][k],max_arr[k+1][j],exp[2*k+1]);
+				long long c = eval(max_arr[i][k],min_arr[k+1][j],exp[2*k+1]);
+				long long d = eval(max_arr[i][k],max_arr[k+1][j],exp[2*k+1]);
+				minVal = min(minVal,min(a,min(b,min(c,d))));
+				maxVal = max(maxVal,max(a,max(b,max(c,d))));
+			}
+			min_arr[i][j] = minVal;
+			max_arr[i][j] = maxVal;
+		}
+	}
+  return max_arr[0][no - 1];
 }
 
 int main() {
